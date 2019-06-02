6d2a179232dd642a0e9eb940c8b6a6f4a66788fe
Trevor Osterman <trevormosterman@gmail.com>
2019-06-02 16:58:02 -0700

    Finished check/uncheck, delete functionality.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 function shoppingList() {
-  // submit button
   $("#js-shopping-list-form").submit(function(event){
     event.preventDefault();
     $("#shopping-list-entry").empty();
     const listEntry = $("#shopping-list-entry").val();
-    //console.log(listEntry);
     $(".shopping-list").append(`<li><span class="shopping-item">${listEntry}</span>
     <div class="shopping-item-controls">
       <button class="shopping-item-toggle">
@@ -14,16 +12,15 @@ function shoppingList() {
         <span class="button-label">delete</span>
       </button>
     </div></li>`);
+    $('#js-shopping-list-form').children('input').val('');
   });
 
-  // 'check' functionality
   $(".shopping-list").on('click','.shopping-item-toggle', function(event) {
-    $(this).closest("span.shopping-item").toggleClass("shopping-item__checked");
+    $(this).parent('.shopping-item-controls').siblings().toggleClass("shopping-item__checked");
   })
 
-  // 'delete' functionality
   $(".shopping-list").on('click','.shopping-item-delete', function(event) {
-    $(this).closest("li").remove();
+    $(this).closest('li').remove();
 
   });
 
