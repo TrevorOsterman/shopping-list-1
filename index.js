00b8fd064dd4be86bdfb6aa6d34828e640379a97
Trevor Osterman <trevormosterman@gmail.com>
2019-06-02 12:31:11 -0700

    Submit button completed

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function shoppingList() {
+  // submit button
   $("#js-shopping-list-form").submit(function(event){
     event.preventDefault();
+    $("#shopping-list-entry").empty();
+    const listEntry = $("#shopping-list-entry").val();
+    console.log(listEntry);
+    $(".shopping-list").append(`<li><span class="shopping-item">${listEntry}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div></li>`);
   })
 }
 
